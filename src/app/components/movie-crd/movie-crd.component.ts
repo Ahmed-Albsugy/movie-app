d4938482796e8184cbe732c9512ed03ac27cee5c
Ahmed <ahmed.albsugy@gmail.com>
2025-05-01 14:19:04 +0300

    add card component

---
+++ src/app/components/movie-crd/movie-crd.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-crd',
+  imports: [],
+  templateUrl: './movie-crd.component.html',
+  styleUrl: './movie-crd.component.css'
+})
+export class MovieCrdComponent {
+
+}
