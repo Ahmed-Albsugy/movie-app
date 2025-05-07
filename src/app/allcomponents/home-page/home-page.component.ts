ac280865b97a8c06c5710399d507417c5959d779
Ahmed <ahmed.albsugy@gmail.com>
2025-05-07 08:32:10 +0300

    start card

--- src/app/allcomponents/home-page/home-page.component.ts
+++ src/app/allcomponents/home-page/home-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { SearchformComponent } from '../searchform/searchform.component';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home-page',
-  standalone:true,
-  imports: [RouterModule,SearchformComponent],
+  standalone: true,
+  imports: [RouterModule, SearchformComponent, CardComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
