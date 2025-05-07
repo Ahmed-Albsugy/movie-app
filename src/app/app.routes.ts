ce1260048c34ea50fc2c70808018bb3fd6f78e4c
hedra talat <hedratalat1717@gmail.com>
2025-05-07 12:54:50 +0300

    moviedetails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent} from './allcomponents/home-page/home-page.component';
+import { HomePageComponent } from './allcomponents/home-page/home-page.component';
 import { WatchlistComponent } from './allcomponents/watchlist/watchlist.component';
 import { LoginComponent } from './allcomponents/login/login.component';
 import { CardComponent } from './allcomponents/card/card.component';
+import { MovieDetailsComponent } from './allcomponents/movie-details/movie-details.component';
 
 export const routes: Routes = [
-  {path:'home-page', component:HomePageComponent},
-  {path:'watchlist',component:WatchlistComponent },
-  {path :'login', component:LoginComponent},
-  {path :'card',component:CardComponent},
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'watchlist', component: WatchlistComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'card', component: CardComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent },
   { path: '', redirectTo: 'home-page', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home-page' }
+  { path: '**', redirectTo: 'home-page' },
 ];
