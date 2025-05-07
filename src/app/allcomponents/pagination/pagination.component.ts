824d834b27e30bf4cb8650f1e7ebec4a4d96bb4e
Ahmed-Albsugy <ahmed.albsugy@gmail.com>
2025-05-07 20:16:08 +0300

    solve conflicts from merging akmal

--- src/app/allcomponents/pagination/pagination.component.ts
+++ src/app/allcomponents/pagination/pagination.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { Input, Output, EventEmitter } from '@angular/core';
-import { MoviesService } from '../services/movies.service';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-pagination',
   imports: [],
   templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.css'
+  styleUrl: './pagination.component.css',
 })
 export class PaginationComponent {
   @Input() currentPage: number = 1;
   @Input() totalPages: number = 1;
   @Output() pageChange = new EventEmitter<number>();
 
-
   constructor(private moviesService: MoviesService) {}
-
 }
