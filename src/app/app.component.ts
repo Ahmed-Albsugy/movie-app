a6dd20c2b28152a249e8fbddfe962843d0162bf7
Aya Ashreey <Ayaashry68@gmail.com>
2025-05-08 11:18:46 +0300

    signin and signup components
    pushed signin and signup components with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomePageComponent } from './allcomponents/home-page/home-page.component';
 import { WatchlistComponent } from './allcomponents/watchlist/watchlist.component';
-import { LoginComponent } from './allcomponents/login/login.component';
 import { CardComponent } from './allcomponents/card/card.component';
 import { HeaderComponent } from './allcomponents/header/header.component';
 import { SearchformComponent } from './allcomponents/searchform/searchform.component';
 import { MovieDetailsComponent } from './allcomponents/movie-details/movie-details.component';
+import { SigninComponent } from './allcomponents/signin/signin.component';
+import { SignupComponent } from './allcomponents/signup/signup.component';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ import { MovieDetailsComponent } from './allcomponents/movie-details/movie-detai
     HomePageComponent,
     SearchformComponent,
     WatchlistComponent,
-    LoginComponent,
+    SigninComponent,
+    SignupComponent,
     CardComponent,
     HeaderComponent,
     MovieDetailsComponent,
