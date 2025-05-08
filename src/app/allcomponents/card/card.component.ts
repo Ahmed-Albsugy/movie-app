c827462a32bd548acc2b48aa21b1603e06bff40e
Ahmed <ahmed.albsugy@gmail.com>
2025-05-08 11:39:50 +0300

    finish card

--- src/app/allcomponents/card/card.component.ts
+++ src/app/allcomponents/card/card.component.ts
@@ -1,28 +1,48 @@
 import { Component, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { SearchformComponent } from "../searchform/searchform.component";
+import { Router, RouterModule } from '@angular/router';
+import { SearchformComponent } from '../searchform/searchform.component';
 import { MoviesService } from '../../services/movies.service';
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  standalone:true,
-  imports: [RouterModule,NgFor],
+  standalone: true,
+  imports: [RouterModule, NgFor, NgIf, NgClass],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
   movies = signal<any[]>([]); // نستخدم signal عشان reactive
 
-  constructor(private moviesService: MoviesService) {
+  constructor(private moviesService: MoviesService, private router: Router) {
     this.moviesService.getNowPlaying(1).then((data) => {
       this.movies.set(data.results); // تملي البيانات في signal
     });
   }
-  
+
   trackById(index: number, movie: any): number {
     return movie.id;
   }
-  
+  //  Dropdown list
+  openedDropdownIndex: number | null = null;
+
+  toggleDropdown(index: number): void {
+    this.openedDropdownIndex =
+      this.openedDropdownIndex === index ? null : index;
+  }
+  // Toggle Favorite
+  moviesList = this.movies.set(
+    this.movies().map((movie) => ({
+      ...movie,
+      favorite: false,
+    }))
+  );
+
+  toggleFavorite(movie: any): void {
+    movie.favorite = !movie.favorite;
+  }
+  // Movie details
+  goToMovieDetails(id: number): void {
+    this.router.navigate(['/movie', id]);
+  }
 }
