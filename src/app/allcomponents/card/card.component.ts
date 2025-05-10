695e0f46bfd44d8364626ae7e0e257ae91f6f356
Aya Ashreey <Ayaashry68@gmail.com>
2025-05-10 19:29:24 +0300

    input name validation

--- src/app/allcomponents/card/card.component.ts
+++ src/app/allcomponents/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { NgClass, NgFor, NgIf } from '@angular/common';
+import { WishlistService } from '../../services/wishlist.service';
 
 @Component({
   selector: 'app-card',
@@ -12,7 +13,7 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 export class CardComponent {
   @Input() movies: any[] = []; // هنا نستقبل movies من الأب
 
-  constructor(private router: Router) {}
+  constructor(private router: Router , private wishlistService: WishlistService) {}
 
   trackById(index: number, movie: any): number {
     return movie.id;
@@ -25,11 +26,27 @@ export class CardComponent {
       this.openedDropdownIndex === index ? null : index;
   }
 
-  toggleFavorite(movie: any): void {
-    movie.favorite = !movie.favorite;
-  }
+  // toggleFavorite(movie: any): void {
+  //   movie.favorite = !movie.favorite;
+  // }
 
   goToMovieDetails(id: number): void {
     this.router.navigate(['/movie', id]);
   }
+
+
+  toggleFavorite(movie: any): void {
+    this.wishlistService.toggleWishlist(movie);
+  }
+
+  isFavorite(movie: any): boolean {
+    return this.wishlistService.isInWishlist(movie.id);
+  }
+
+  
+
+
+
+
+
 }
