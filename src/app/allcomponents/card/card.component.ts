bedb36e1f3f7867933922f6125ece390a8f625bd
Aya Ashreey <Ayaashry68@gmail.com>
2025-05-10 20:23:11 +0300

    add fav movie to firestore database

--- src/app/allcomponents/card/card.component.ts
+++ src/app/allcomponents/card/card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { NgClass, NgFor, NgIf } from '@angular/common';
-import { WishlistService } from '../../services/wishlist.service';
+import { FavMoviesService } from '../../services/fav-movies.service';
 
 @Component({
   selector: 'app-card',
@@ -12,14 +12,15 @@ import { WishlistService } from '../../services/wishlist.service';
 })
 export class CardComponent {
   @Input() movies: any[] = []; // هنا نستقبل movies من الأب
+  openedDropdownIndex: number | null = null;
 
-  constructor(private router: Router , private wishlistService: WishlistService) {}
+
+  constructor(private router: Router, private favMoviesService: FavMoviesService) { }
 
   trackById(index: number, movie: any): number {
     return movie.id;
   }
 
-  openedDropdownIndex: number | null = null;
 
   toggleDropdown(index: number): void {
     this.openedDropdownIndex =
@@ -30,20 +31,32 @@ export class CardComponent {
   //   movie.favorite = !movie.favorite;
   // }
 
+  async toggleFavorite(movie: any): Promise<void> {
+    movie.favorite = !movie.favorite;
+    console.log(`Toggling favorite for: ${movie.title}, Now: ${movie.favorite}`);
+
+    try {
+      if (movie.favorite) {
+        await this.favMoviesService.addToFavorites(movie);
+      } else {
+        await this.favMoviesService.removeFromFavoritesById(movie.id);
+      }
+    } catch (err) {
+      console.error('🔥 Error in toggleFavorite:', err);
+    }
+  }
+
   goToMovieDetails(id: number): void {
     this.router.navigate(['/movie', id]);
   }
 
 
-  toggleFavorite(movie: any): void {
-    this.wishlistService.toggleWishlist(movie);
-  }
+  // toggleFavorite(movie: any): void {
+  //   this.wishlistService.toggleWishlist(movie);
+  // }
 
-  isFavorite(movie: any): boolean {
-    return this.wishlistService.isInWishlist(movie.id);
-  }
 
-  
+
 
 
 
